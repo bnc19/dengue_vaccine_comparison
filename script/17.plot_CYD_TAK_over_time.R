3464dcd338b66ef6b7d2403e0dab9dedbf8fabdf
bnc19 <bnc19@ic.ac.uk>
2024-02-22 10:49:06 +0000

    update colours, best models and tweak plots

--- script/17.plot_CYD_TAK_over_time.R
+++ script/17.plot_CYD_TAK_over_time.R
@@ -1,22 +1,27 @@
 # Plot VE of Dengvaxia and Qdenga side by side
+rm(list = ls())
 library(tidyverse)
-source("R/plotting_functions.R")
-n= 100
+source("compare_vaccines/R/plotting_functions.R")
 
-TAK_file = "/Users/bethancracknelldaniels/Desktop/R projects/Qdenga_efficacy/output/final/M32/VE.RDS"
-CYD_file = "/Users/bethancracknelldaniels/Desktop/R projects/CYD_Efficacy/output/M10/VE.RDS"
+# colours 
+mycols = c("#9999FF", "#FF9900")
+
+# files 
+TAK_file = "TAK/output/M2/VE.RDS"
+CYD_file = "CYD/output/M12/VE.RDS"
+
 set.seed(123)
-CYD_VE = as.data.frame(sapply(readRDS(CYD_file), sample, n))
-TAK_VE = as.data.frame(sapply(readRDS(TAK_file), sample, n))
+
+CYD_VE = readRDS(CYD_file)
+TAK_VE = readRDS(TAK_file)
 
 # VE 
 C_VE_model = extract_model_results(CYD_VE)  
 T_VE_model = extract_model_results(TAK_VE)  
-C_VE_model$vaccine = "CYD"
-T_VE_model$vaccine = "TAK"
+C_VE_model$vaccine = "Dengvaxia"
+T_VE_model$vaccine = "Qdenga"
 
 # combine both vaccines
-
 comb_VE = C_VE_model %>%
   bind_rows(T_VE_model) %>% 
   filter(group == "VE_BKRT") %>%  
@@ -27,16 +32,10 @@ comb_VE = C_VE_model %>%
          serotype = factor(serotype, levels = 1:4, 
                            labels = c(paste0("DENV", 1:4))),
          outcome = factor(outcome, levels = 1:2, 
-                          labels = c("symptomatic", "hospitalised")), 
-         vaccine = factor(vaccine, levels = c("CYD", "TAK"), 
-                          labels = c("Dengvaxia", "Qdenga")))
-
-
-mycols = c("#9999FF", "#FF9900")
+                          labels = c("symptomatic", "hospitalised")))
 
 plot_ve_over_time = comb_VE %>%   
   filter(month < 55) %>% 
-  filter(serostatus != "multitypic") %>% 
   ggplot(aes(x = month, y = mean)) +
   geom_line(aes(color = vaccine)) +
   geom_ribbon(
@@ -46,9 +45,9 @@ plot_ve_over_time = comb_VE %>%
       fill = vaccine),
     alpha = 0.5
   ) +
-  labs(x = "Month", y = "Vaccine Efficacy (%)") +
+  labs(x = "Month", y = "Vaccine efficacy (%)") +
   geom_hline(yintercept=0, linetype="dashed",color = "black", linewidth=1) +
-  facet_grid(serotype~outcome+serostatus) + 
+  facet_grid(serostatus + outcome ~ serotype, scales = "free") + 
   theme_light() + 
   theme(plot.title = element_text(hjust = 0.5),
         legend.position = "top")+
@@ -58,7 +57,7 @@ plot_ve_over_time = comb_VE %>%
 
 ggsave(
   plot = plot_ve_over_time,
-  filename = "output/VE_overtime.jpg",
+  filename = "compare_vaccines/output/VE_overtime.jpg",
   height = 20,
   width = 30,
   units = "cm",
